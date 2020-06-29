16443205ef47ca5ce2ceffd87de745413ea45ea0
hwendland <wendland.hannah@gmail.com>
2020-06-29 07:19:20 +0200

    make task page work

--- src/components/Editor/Task.vue
+++ src/components/Editor/Task.vue
@@ -1,16 +1,32 @@
 <template>
-<div><h1>{{task.title}}</h1></div>
+  <div class="col">
+    <div class="d-flex justify-content-end mb-5" style="flex-grow: 0">
+      <span @click="addWidget('mc-widget')" class="material-icons">check_box_outline_blank</span>
+      <span @click="addWidget('single-choice-widget')" class="material-icons">radio_button_unchecked</span>
+      <span class="material-icons">star_half</span>
+      <span class="material-icons">question_answer</span>
+    </div>
+    <draggable :list="widgets" class="row">
+      <component @delete="deleteWidget(widget.id)" v-model=widget.content
+                 :size="widget.columns" @updateSize="widget.columns=$event"
+                 :is="widget.type" v-for="(widget) in widgets" :key="widget.id"></component>
+    </draggable>
+  </div>
 </template>
 
 <script>
+import { widgetMethods } from '../../mixins/Editor/widgetMethods'
+import draggable from 'vuedraggable'
+import SingleChoiceWidget from './Widgets/SingleChoice'
 
 export default {
   name: 'CourseTask',
   props: ['chapterId', 'contentId'],
+  components: { draggable, SingleChoiceWidget },
+  mixins: [widgetMethods],
   computed: {
-    task () {
-      return this.$store.state.editor.course.getContent(this.chapterId, this.contentId)
-    }
+    task () { return this.$store.state.editor.course.getContent(this.chapterId, this.contentId) },
+    widgets () { return this.task.widgets }
   }
 }
 </script>
